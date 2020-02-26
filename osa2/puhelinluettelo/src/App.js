321f07c06db95df88468da2e3871f7c02b07b869
Antti Eskelinen <antti.eskelinen3@gmail.com>
2020-02-26 18:45:37 +0200

    2.10

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -8,6 +8,42 @@ const nameAlreadyExists = (persons, name) => {
   return persons.map(p=>p.name).includes(name)
 }
 
+const Filter = ({filter, onFilterChange}) => {
+  return (
+    <p>filter shown with <input value={filter} onChange={onFilterChange}/></p>
+  )
+}
+
+const PersonForm = (props) => (
+
+  <form onSubmit={props.handleAddPerson}>
+    <div>
+      name: <input value={props.newName} onChange={props.handleNameChange} />
+    </div>
+    <div>
+      number: <input value={props.newNumber} onChange={props.handleNumberChange} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+)
+
+const Persons = ({persons, filter}) => {
+  const caseinsensitiveFilter = (person) => (
+    person.name.toUpperCase().includes(
+      filter.toUpperCase()
+    )
+  )
+  return (
+    <ul>
+      {persons
+        .filter(caseinsensitiveFilter)
+        .map(person => <Number key={person.name} person={person}/>)}
+    </ul>
+  )
+}
+
 const App = () => {
 
   const [ persons, setPersons] = useState([
@@ -20,22 +56,17 @@ const App = () => {
       number: '123445'
     }
   ])
-
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+
+
   const [ filter, setFilter ] = useState('')
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
   const handleFilterChange = (event) => setFilter(event.target.value)
 
-  const caseinsensitiveFilter = (person) => (
-    person.name.toUpperCase().includes(
-      filter.toUpperCase()
-    )
-  )
-
-  const handleAddName = (event) => {
+  const handleAddPerson = (event) => {
     event.preventDefault()
     if (nameAlreadyExists(persons, newName)) {
       alert(`${newName} is already added to phonebook`)
@@ -53,28 +84,20 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-
-      filter shown with <input value={filter} onChange={handleFilterChange}/>
+      <Filter filter={filter} onFilterChange={handleFilterChange}/>
 
       <h2>add a new</h2>
-      <form onSubmit={handleAddName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+        handleAddPerson={handleAddPerson}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        newName={newName}
+        newNumber={newNumber}
+      />
 
       <h2>Numbers</h2>
-      <ul>
-        {persons
-          .filter(caseinsensitiveFilter)
-          .map(person => <Number key={person.name} person={person}/>)}
-      </ul>
+      <Persons persons={persons} filter={filter}/>
+
     </div>
   )
 
