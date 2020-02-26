48407aa2662003750c1ab6a56c6913bbc834b9da
Antti Eskelinen <antti.eskelinen3@gmail.com>
2020-02-26 22:03:24 +0200

    2.14*

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 import axios from 'axios'
 
 const COUNTRY_RESOURCE = 'https://restcountries.eu/rest/v2/all';
+const WEATHER_RESOURCE = 'https://api.openweathermap.org/data/2.5/weather?units=metric'
+const WEATHER_RESOURCE_CITY_PARAM = 'q'
+const WEATHER_RESOURCE_APPID_PARAM = 'appid'
+const WEATHER_ICON_RESOURCE = 'http://openweathermap.org/img/wn/{icon}@2x.png'
+
 
 const Filter = ({filter, handleFilterChanged}) => {
   return (
@@ -61,8 +66,44 @@ const CountryDetails = ({country}) => (
         {country.languages.map(l => <li key={l.name}>{l.name}</li>)}
       </ul>
     <img src={country.flag} width="200" alt=""/>
+    <Weather country={country}/>
   </>
 )
+
+const Weather = ({country}) => {
+
+  const [weather, setWeather] = useState(undefined)
+  const [iconUrl, setIconUrl] = useState('')
+  const weatherUrl = new URL(WEATHER_RESOURCE);
+  weatherUrl.searchParams.append(WEATHER_RESOURCE_CITY_PARAM, country.capital);
+  const API_KEY = process.env.REACT_APP_API_KEY
+  console.log(process.env)
+  weatherUrl.searchParams.append(WEATHER_RESOURCE_APPID_PARAM, API_KEY)
+
+  useEffect(() => {
+    console.log("requesting")
+    axios
+      .get(weatherUrl)
+      .then(response => {
+        const icon = response.data.weather[0].icon
+        const url = WEATHER_ICON_RESOURCE.replace("{icon}", icon)
+        setWeather(response.data)
+        setIconUrl(url)
+      })
+    }, [weatherUrl])
+  if (weather) {
+    return (
+      <>
+        <h2>Weather in {country.capital}</h2>
+        <p><b>temperature:</b> {weather.main.temp} Celsius</p>
+        <img src={iconUrl} alt='' />
+        <p><b>wind:</b> {weather.wind.speed} direction {weather.wind.deg} degrees</p>
+      </>
+    )
+  } else {
+    return(<></>)
+  }
+}
 
 const App = () => {
   const [countries, setCountries] = useState([])
